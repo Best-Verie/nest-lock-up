22f0a194112e9146d2d5f0e9f195ab0da0104c4b
Best Verie Iradukunda <60257249+Best-Verie@users.noreply.github.com>
2021-07-16 12:51:06 +0200

    added swagger documentations

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('Events tickets reservation API')
+    .setVersion('1.0')
+    .addTag('venues')
+    .addTag('events')
+    .addTag('eventCategories')
+    .addTag('ticketCategories')
+    .addTag('tickets')
+    .addTag('userCategories')
+    .addTag('users')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
