ca72fa6c3b8bcf99864aa29d9b272850b1589f37
Best Verie Iradukunda <60257249+Best-Verie@users.noreply.github.com>
2021-07-17 17:09:50 +0200

    users

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,8 +7,11 @@ import { EventsModule } from './events/events.module';
 import { TicketCategoriesModule } from './ticket-categories/ticket-categories.module';
 import { TicketsModule } from './tickets/tickets.module';
 import { DatabaseModule } from './config/database/database.module';
+import { UsersModule } from './users/users/users.module';
+import { UusersController } from './uusers/uusers.controller';
 
 @Module({
-  imports: [VenuesModule, EventCategoriessModule, EventsModule, TicketCategoriesModule, TicketsModule, DatabaseModule],
+  imports: [VenuesModule, EventCategoriessModule, EventsModule, TicketCategoriesModule, TicketsModule, DatabaseModule, UsersModule],
+  controllers: [UusersController],
 })
 export class AppModule {}
