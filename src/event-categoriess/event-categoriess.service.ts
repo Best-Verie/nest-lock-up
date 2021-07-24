01f3b4051a079ad460e9363698458231d205f283
Best Verie Iradukunda <60257249+Best-Verie@users.noreply.github.com>
2021-07-24 12:13:00 +0200

    ticket categoriees validation

--- src/event-categoriess/event-categoriess.service.ts
+++ src/event-categoriess/event-categoriess.service.ts
@@ -1,28 +1,44 @@
-import { EventCategoriesDto } from './Dto/event.categories.dto';
-import { EventCategoriesInterface } from './intefaces/event-category.inteface';
-import { Inject, Injectable } from '@nestjs/common';
-import { Model } from 'mongoose';
+import {
+  EventCategoriesDto
+} from './Dto/event.categories.dto';
+import {
+  EventCategoriesInterface
+} from './intefaces/event-category.inteface';
+import {
+  Inject,
+  Injectable
+} from '@nestjs/common';
+import {
+  Model
+} from 'mongoose';
 
 @Injectable()
 export class EventCategoriessService {
-    constructor( @Inject('EventCategories_Model')
-    private eventCategoriesModel: Model<EventCategoriesInterface>,
-){}
+  constructor(@Inject('EventCategories_Model') private eventCategoriesModel: Model < EventCategoriesInterface > , ) {}
 
- async createEventCategory(createEventCategoryDto: EventCategoriesDto): Promise<EventCategoriesDto> {
+  async createEventCategory(createEventCategoryDto: EventCategoriesDto): Promise < EventCategoriesDto > {
+    const foundEventCategory = await (await this.eventCategoriesModel.findOne({
+      'name': EventCategoriesDto.name
+    })).execPopulate();
+
+    if (foundEventCategory) {
+      throw new Error("Cant save the same event category twice! (same title)");
+    }
     const createdEventCategory = new this.eventCategoriesModel(createEventCategoryDto);
     return createdEventCategory.save();
   }
 
 
-async getAllEventCategories(){
+  async getAllEventCategories() {
     return this.eventCategoriesModel.find();
-}
-async updateEventCategory(id:string, EventCategoryDto:EventCategoriesDto): Promise<EventCategoriesDto>{
-  return await this.eventCategoriesModel.findByIdAndUpdate(id,EventCategoryDto, {new:true})
-}
+  }
+  async updateEventCategory(id: string, EventCategoryDto: EventCategoriesDto): Promise < EventCategoriesDto > {
+    return await this.eventCategoriesModel.findByIdAndUpdate(id, EventCategoryDto, {
+      new: true
+    })
+  }
 
-async deleteEventCategory(id:string):Promise<EventCategoriesDto>{
-  return await this.eventCategoriesModel.findByIdAndRemove(id);
-}
+  async deleteEventCategory(id: string): Promise < EventCategoriesDto > {
+    return await this.eventCategoriesModel.findByIdAndRemove(id);
+  }
 }
